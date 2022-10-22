10c9749d142ea42293dd880a0a74d7bd9a8ef7f4
vajadevdatt3@gmail.com <vajadevdatt3@gmail.com>
2022-10-22 13:42:15 +0530

    inital commit]

---
+++ src/Login.js
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+
+
+export default function Login(props) {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+        console.log(pass);
+    }
+  return (
+    <div>
+      <h2>Login</h2>
+            <form className="login-form" onSubmit={handleSubmit}>
+                <label htmlFor="email">email</label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="Email" id="email" name="email" />
+                <label htmlFor="password">password</label>
+                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="Password" id="password" name="password" />
+                <button type="submit">Log In</button>
+            </form>
+            <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
+    </div>
+  )
+}
